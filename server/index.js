ac873c58a075bdc9fd8ecc3366755ed42dd29fd3
Moise1 <rwibutso.mo@gmail.com>
2025-04-10 22:39:48 +0200

    edit render.yaml

--- server/index.js
+++ server/index.js
@@ -1,35 +1,28 @@
 
+const path = require("path");
+require('dotenv').config({ path: path.join(__dirname, './.env') });
+
 const express = require('express');
 const mongoose = require('mongoose');
 
 const cors = require('cors');
-const path = require("path");
 
 const memberRoutes = require('./routes/members');
 const sessionRoutes = require('./routes/sessions');
 const statsRoutes = require('./routes/stats');
 const authRoutes = require('./routes/auth');
 
-require('dotenv').config({ path: path.join(__dirname, '../.env') });
-
-
-const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors({
-    origin: 'https://choir-attendance-tracker.onrender.com',
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+
+app.use(cors());
 app.use(express.json());
 
-
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('DB Connected...'))
 .catch((err) => console.error('Connection failed...', err));
+
+const port = process.env.PORT || 5000;
 
 
 app.use('/api/v1', authRoutes);
