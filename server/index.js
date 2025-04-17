86947c6c4700aa00cf882fc8232199113ec580f9
Moise1 <rwibutso.mo@gmail.com>
2025-04-17 11:40:05 +0200

    handle static files

--- server/index.js
+++ server/index.js
@@ -31,7 +31,17 @@ app.use('/api/v1', sessionRoutes);
 app.use('/api/v1', statsRoutes);
 
 
+// Serve React frontend
+// app.use(express.static(path.join(__dirname, '../client/build')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
+
 
